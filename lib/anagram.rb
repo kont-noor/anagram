b0905e79d0006a723c5a75c8164e622b52fd0ba0
Nickolay Kondratenko <devmarkup@gmail.com>
2016-11-02 19:11:45 +0200

    add basic anagram functionality

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,5 +1,19 @@
 require "anagram/version"
 
 module Anagram
-  # Your code goes here...
+  def self.check(first_string, second_string)
+    if anagram?(first_string, second_string)
+      "#{first_string} is an anagram of #{second_string}"
+    else
+      "#{first_string} is NOT an anagram of #{second_string}"
+    end
+  end
+
+  def self.anagram?(first_string, second_string)
+    sorted_string_chars(first_string) == sorted_string_chars(second_string)
+  end
+
+  def self.sorted_string_chars(string)
+    string.sub(/\s/, '').scan(/\w/).map(&:downcase).sort
+  end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,11 +1,54 @@
 require 'spec_helper'
 
 describe Anagram do
-  it 'has a version number' do
-    expect(Anagram::VERSION).not_to be nil
+  describe '::check' do
+    it "writes correct message for anagram" do
+      allow(subject).to receive(:anagram?).and_return(true)
+
+      expect(subject.check('a', 'b')).to eq('a is an anagram of b')
+    end
+
+    it "writes incorrect message for not anagram" do
+      allow(subject).to receive(:anagram?).and_return(false)
+
+      expect(subject.check('a', 'b')).to eq('a is NOT an anagram of b')
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '::anagram?' do
+    let(:correct_anagrams) do
+      [
+        ["wisdom", "mid sow"],
+        ["Seth Rogan", "Gathers No"],
+        ["Schoolmaster", "The classroom"],
+        ["Vacation Times", "I'm Not as Active"],
+      ]
+    end
+
+    let(:incorrect_anagrams) do
+      [
+        ["Reddit", "Eat Dirt"],
+        ["Astronomers", "Moon starer"],
+        ["Dormitory", "Dirty Rooms"]
+      ]
+    end
+
+    it 'is true for correct couples of words' do
+      correct_anagrams.each do |couple|
+        expect(subject.anagram?(*couple)).to eq(true)
+      end
+    end
+
+    it 'is false for incorrect ones' do
+      incorrect_anagrams.each do |couple|
+        expect(subject.anagram?(*couple)).to eq(false)
+      end
+    end
+  end
+
+  describe '::sorted_string_chars' do
+    it 'returns all chars from string sorted alfabeticaly' do
+      expect(subject.sorted_string_chars('wisdom')).to eq(%w(d i m o s w))
+    end
   end
 end
